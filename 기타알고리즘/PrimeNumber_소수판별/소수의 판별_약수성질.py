298243e35faa01a2eb867b06c57492056f161c06
Wangi-C <65946357+Wangi-C@users.noreply.github.com>
2021-02-16 22:08:20 +0900

    Add files via upload

---
+++ 기타알고리즘/PrimeNumber_소수판별/소수의 판별_약수성질.py
@@ -0,0 +1,9 @@
+import math
+
+# 소수 판별 함수
+def is_prime_num(x):
+    #2부터 (x-1)까지의 모든 수를 확인
+    for i in range(2, int(math.sqrt(x)) + 1):
+        if x % i == 0:
+            return False
+    return True
