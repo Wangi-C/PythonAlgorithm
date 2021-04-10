86ed82506b900e4f6b0393f9b419bdf39bf60d88
Wangi-C <whdhksrl455@naver.com>
2021-04-10 21:31:29 +0900

    Add Stack_2 [add]

--- Baekjoon_Q/Stack_2.py
+++ Baekjoon_Q/Stack_2.py
@@ -1,15 +1,18 @@
-#제로
-num = []
-n = int(input())
-result = 0
+# #제로
+# num = []
+# n = int(input())
+# result = 0
 
-for i in range(n):
-    data = int(input())
-    if data != 0:
-        num.append(data)
-        result += data
-    else:
-        result -= num[len(num) - 1]
-        num.pop(len(num)-1)
+# for i in range(n):
+#     data = int(input())
+#     if data != 0:
+#         num.append(data)
+#         result += data
+#     else:
+#         result -= num[len(num) - 1]
+#         num.pop(len(num)-1)
 
-print(result)
+# print(result)
+
+def add(a, b):
+    return a+b
